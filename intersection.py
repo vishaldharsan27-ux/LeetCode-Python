a9dd9cae81324af5ba00f67b638900ab499dc515
Vishal Dharsan S.R <vishaldharsan@gmail.com>
2026-06-24 01:38:49 +0530

    intersection of two array

---
+++ intersection.py
@@ -0,0 +1,7 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        s1=set(nums1)
+        s2=set(nums2)
+        x=s1.intersection(s2)
+        l=list(x)
+        return l
